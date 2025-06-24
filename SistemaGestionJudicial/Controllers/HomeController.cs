19108be49efc519c9b2d3157f1ff43b502ece62d
Marcos <marcosandres356@gmail.com>
2025-06-24 05:07:14 -0500

    Primer commit

---
+++ SistemaGestionJudicial/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SistemaGestionJudicial.Models;
+
+namespace SistemaGestionJudicial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Delincuentes()
+        {
+            return View();
+        }
+
+        public IActionResult Crimenes()
+        {
+            return View();
+        }
+
+        public IActionResult Casos()
+        {
+            return View();
+        }
+
+        public IActionResult Jueces()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
