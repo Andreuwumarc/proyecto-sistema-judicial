8c9cd03bbe4e48ddf3529fb1b7086e2e42321360
Marcos <marcosandres356@gmail.com>
2025-07-16 05:50:31 -0500

    login

--- SistemaGestionJudicial/Controllers/HomeController.cs
+++ SistemaGestionJudicial/Controllers/HomeController.cs
@@ -20,23 +20,14 @@ namespace SistemaGestionJudicial.Controllers
         }
 
 
-
         public IActionResult CrearDenuncia()
         {
             return View();
         }
 
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-
+        
         public IActionResult RecuperacionCuenta()
-        {
-            return View();
-        }
-        public IActionResult Token()
         {
             return View();
         }
